d6fd5ad1ded2c81eab7e5c834a7d50037757879c
BrianStanwood <brian.stanwood@gmail.com>
2018-11-05 21:23:07 -0500

    Add files via upload

--- bitcoin+project.py
+++ bitcoin+project.py
@@ -1,10 +1,12 @@
 # Project Completed by Brian Stanwood, Minhquan Nguyen, Luke Deviney, Liam Byrne on 4/17/2018
-# This project finds the past Bitcoin values from spreedsheet "BTC.csv" and pridicts its average for the future as well
+# This project finds the past Bitcoin values from spreedsheet "BTC.csv" and predicts its average for the future as well
 #   as displays graphical current results found. 
 
 # Step 1
 ###### importing 
 import pandas as pd
+import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 import re
 
@@ -20,28 +22,29 @@ Y = bitcoin["open"]
 
 # finds average price for red line plot (predicted price line)
 averagePrice = sum(Y) / len(Y)
-X_predict = pd.DataFrame([[X[0]], [len(X)]])
-Y_predict = pd.DataFrame([[Y[0]], [averagePrice]])
+x_predict = pd.DataFrame([[X[0]], [len(X)]])
+y_predict = pd.DataFrame([[Y[0]], [len(Y)]])
+x = np.array(x_predict)
+y = np.array(y_predict)
+
+############# find slope and intercept of line for calculations using linear regression from scipy
+slope, intercept, r_value, p_value, std_err = stats.linregress(x[:,0],y[:,0])
 
 # In[58]:
 
-#### plot graph with blue dots (data marks) and red line (average estimate line) with matplotlib
-
 # Step 2
 # makes grid and plots points/line
+#### plot graph with blue dots (data marks) and red line (average estimate line) with matplotlib
 plt.rcParams['axes.labelsize'] = 14
 plt.rcParams['xtick.labelsize'] = 12
 plt.rcParams['ytick.labelsize'] = 12
-plt.plot(X_predict, Y_predict, "r-")
+plt.plot(x, y, "r-")
 plt.plot(X, Y, "b.")
 plt.xlabel("$Time (inDays)$", fontsize=16)
 plt.ylabel("$Price$", rotation=0, fontsize=16)
 plt.axis([0, 2000, 0, 30000])
 plt.show()
 
-
-############# find slope of line for future calculations
-slope = (averagePrice - Y[0])/(len(X) - X[0]) 
 
 # Step 3
 ############################ user interface portion
@@ -71,15 +74,15 @@ while dateOrPrice != 3:
                 added = added + (float(dateArray[0]) + 28)      
         else:
             print('Date given is before Bitcoin timeframe.')
-        hitPrice = float(added) * float(slope) + Y[0]
+        hitPrice = float(added) * float(slope) + intercept
         print('The price of the Bitcoin estimated average is around ' , '${:,.2f}'.format(hitPrice) , 'at that time.\n')
         
     # find day by price 
     elif dateOrPrice == '2':
         price = input('Enter a price to see what day it may occur: $')
         price = price.replace(",", "")
-        hitDate = (float(price)-Y[0])/float(slope)
-        print('The day Bitcoin will hit this average price is approximately : ' , (int(hitDate)), ' days from 4/28/2018.\n')
+        hitDate = (float(price)-intercept)/float(slope)
+        print('The day Bitcoin will hit this average price is approximately : ' , (int(hitDate)), ' days from 4/28/2013.\n')
 
     # exit    
     elif dateOrPrice == '3':
